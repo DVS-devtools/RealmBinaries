961313153436c517a0fa8eacf3c62b43dc3ab731
Moreno Nicolelli <morenonicolelli@gmail.com>
2023-04-11 17:39:02 +0200

    update realm version

--- Realm.xcframework/watchos-arm64_i386_x86_64-simulator/Realm.framework/PrivateHeaders/RLMMongoCollection_Private.h
+++ Realm.xcframework/watchos-arm64_i386_x86_64-simulator/Realm.framework/PrivateHeaders/RLMMongoCollection_Private.h
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////
 //
-// Copyright 2016 Realm Inc.
+// Copyright 2020 Realm Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,26 +16,22 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
-#import <Realm/RLMSyncUtil.h>
+#import <Realm/RLMMongoCollection.h>
 
-#import <Realm/RLMProperty.h>
-#import <Realm/RLMRealmConfiguration.h>
-#import <Realm/RLMCredentials.h>
+RLM_HEADER_AUDIT_BEGIN(nullability)
 
 @class RLMUser;
 
-typedef void(^RLMSyncCompletionBlock)(NSError * _Nullable, NSDictionary * _Nullable);
-typedef void(^RLMSyncBasicErrorReportingBlock)(NSError * _Nullable);
+@interface RLMMongoCollection ()
+- (instancetype)initWithUser:(RLMUser *)user
+                 serviceName:(NSString *)serviceName
+                databaseName:(NSString *)databaseName
+              collectionName:(NSString *)collectionName;
 
-typedef NSString* RLMServerPath;
+- (RLMChangeStream *)watchWithMatchFilter:(nullable id<RLMBSON>)matchFilter
+                                 idFilter:(nullable id<RLMBSON>)idFilter
+                                 delegate:(id<RLMChangeEventDelegate>)delegate
+                                scheduler:(void (^)(dispatch_block_t))scheduler;
+@end
 
-NS_ASSUME_NONNULL_BEGIN
-
-extern NSString *const kRLMSyncErrorStatusCodeKey;
-extern NSString *const kRLMSyncUnderlyingErrorKey;
-
-#define RLM_SYNC_UNINITIALIZABLE \
-- (instancetype)init __attribute__((unavailable("This type cannot be created directly"))); \
-+ (instancetype)new __attribute__((unavailable("This type cannot be created directly")));
-
-NS_ASSUME_NONNULL_END
+RLM_HEADER_AUDIT_END(nullability)
